28d42fa01b97ed04efd469d8c70c9a482f568c0b
Ruchir Mital Parekh <ruchir.m.parekh@gmail.com>
2026-08-21 02:09:01 +0530

    Fix GitHub Pages deployment basePath and workflow permissions

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let repo = '';
+if (isGithubActions && process.env.GITHUB_REPOSITORY) {
+  const repoName = process.env.GITHUB_REPOSITORY.split('/')[1];
+  if (repoName) {
+    repo = `/${repoName}`;
+  }
+}
+
 const nextConfig = {
   output: 'export',
+  basePath: repo || undefined,
+  assetPrefix: repo ? `${repo}/` : undefined,
   images: {
     unoptimized: true,
     remotePatterns: [
