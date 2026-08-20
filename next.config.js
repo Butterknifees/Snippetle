69e54e05a84e3dd527a7b2b18da777b4d3d4da27
Ruchir Mital Parekh <ruchir.m.parekh@gmail.com>
2026-08-21 02:15:00 +0530

    Add .nojekyll and update workflow build stages

--- next.config.js
+++ next.config.js
@@ -1,17 +1,8 @@
 /** @type {import('next').NextConfig} */
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-let repo = '';
-if (isGithubActions && process.env.GITHUB_REPOSITORY) {
-  const repoName = process.env.GITHUB_REPOSITORY.split('/')[1];
-  if (repoName) {
-    repo = `/${repoName}`;
-  }
-}
-
 const nextConfig = {
   output: 'export',
-  basePath: repo || undefined,
-  assetPrefix: repo ? `${repo}/` : undefined,
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || undefined,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH ? `${process.env.NEXT_PUBLIC_BASE_PATH}/` : undefined,
   images: {
     unoptimized: true,
     remotePatterns: [
